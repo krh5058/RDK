a044f829b71d00efefd7649469f2d4fa6d88fd25
krh5058 <ken.r.hwang@gmail.com>
2012-12-06 13:01:20 -0500

    Presentation set-up for display purposes.

--- rdkMain.m
+++ rdkMain.m
@@ -1,6 +1,9 @@
 sys = ObjSet.SysCheck;
 exp = ObjSet.ExpSet(sys);
 obj = ObjSet(sys,exp);
+dot = obj.DotGen;
+w = Screen('OpenWindow',1,0,[],[],[],4);
+for i = 1:60;obj.exp.draw_fun(dot(:,:,i,1)',w);obj.exp.selectstereo_fun(w,1);obj.exp.draw_fun(dot(:,:,i,2)',w);obj.exp.flip_fun(w);end;
 
 % j = batch('testhi', 2, {obj},'matlabpool',1,'FileDependencies','ObjSet.m pathdef.m');
 % 
